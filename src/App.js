923c85850a84baf1ab8e25e0c4759b85c29b0278
Ana Luisa <ana.luisa.isa@hotmail.com>
2023-05-03 21:10:07 -0300

    feat(Header): Implementação do header da aplicação utilizando o tailwind

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
+import Header from "./components/Header";
+
 function App() {
   return (
-    <div className="bg-blue-500 text-white p-4">
-      <h1 className="text-2xl font-bold">Meu portifólio!!</h1>
-      <p className="mt-2">Alguma coisa aqui...</p>
-    </div>
+    <Header />
   );
 }
 
