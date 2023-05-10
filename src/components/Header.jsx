608174d43e4fa5db132ec23156eeb13349ef9b41
Ana Luisa <ana.luisa.isa@hotmail.com>
2023-05-09 22:41:56 -0300

    feat: Componente Header, retirando logo da pasta e colocando na pasta de imagens

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,58 +1,99 @@
-import logo from "./logo.png";
+import { Disclosure } from '@headlessui/react';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
+import logo from "../images/logo.png";
 
-function Header() {
+const navigation = [
+  { name: 'Início', href: '/inicio', current: true },
+  { name: 'Sobre', href: '/sobre', current: false },
+  { name: 'Projetos', href: '/projetos', current: false },
+  { name: 'Contato', href: '/contato', current: false },
+];
 
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ');
+}
+
+export default function Header() {
   return (
-    <nav class="bg-gray-800">
-      <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-        <div class="relative flex h-16 items-center justify-between">
+    <Disclosure as="nav" className="bg-black bg-opacity-50">
+      {({ open }) => (
+        <>
+          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
 
-          <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
-            <button type="button" class="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
-              <span class="sr-only">Open main menu</span>
+            <div className="relative flex h-16 items-center justify-between">
+              <div className="inset-y-0 left-0 flex items-center sm:hidden">
+                {/* Mobile menu button*/}
+                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
 
-              <svg class="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-              </svg>
+              <div className="flex flex-1 items-center justify-between sm:items-stretch">
+                <div className="flex flex-shrink-0 items-center">
+                  <img
+                    className="block h-8 w-auto lg:hidden rounded-lg"
+                    src={logo}
+                    alt="Nome Ana Luisa"
+                  />
+                  <img
+                    className="hidden h-8 w-auto lg:block rounded-lg"
+                    src={logo}
+                    alt="Nome Ana Luisa"
+                  />
+                </div>
 
-              <svg class="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-              </svg>
-            </button>
-          </div>
+                <div className="hidden sm:ml-6 sm:block">
+                  <div className="flex space-x-4">
+                    {navigation.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className={classNames(
+                          item.current
+                            ? 'bg-gray-900 text-white'
+                            : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                          'rounded-md px-3 py-2 text-sm font-medium',
+                        )}
+                        aria-current={item.current ? 'page' : undefined}
+                      >
+                        {item.name}
+                      </a>
+                    ))}
+                  </div>
+                </div>
 
-          <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-between">
-            <div class="flex flex-shrink-0 items-center">
-              <img class="block h-8 w-auto lg:hidden" src={logo} alt="Nome Ana Luisa" />
-              <img class="hidden h-8 w-auto lg:block" src={logo} alt="Nome Ana Luisa" />
-            </div>
-
-            <div class="hidden sm:ml-6 sm:block">
-              <div class="flex space-x-4">
-                <a href="/inicio" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Início</a>
-                <a href="/sobre" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Sobre</a>
-                <a href="/projetos" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Projetos</a>
-                <a href="/contato" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Contato</a>
               </div>
             </div>
           </div>
-        </div>
-      </div>
 
-      <div class="sm:hidden" id="mobile-menu">
+          <Disclosure.Panel className="sm:hidden">
+            <div className="space-y-1 px-2 pb-3 pt-2">
+              {navigation.map((item) => (
+                <Disclosure.Button
+                  key={item.name}
+                  as="a"
+                  href={item.href}
+                  className={classNames(
+                    item.current
+                      ? 'bg-gray-900 text-white'
+                      : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                    'block rounded-md px-3 py-2 text-base font-medium',
+                  )}
+                  aria-current={item.current ? 'page' : undefined}
+                >
+                  {item.name}
+                </Disclosure.Button>
+              ))}
+            </div>
 
-        <div class="space-y-1 px-2 pb-3 pt-2">
-          <a href="/inicio" class="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium" aria-current="page">Início</a>
-          <a href="/sobre" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Sobre</a>
-          <a href="/projetos" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Projetos</a>
-          <a href="/contato" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Contato</a>
-        </div>
-
-      </div>
-    </nav>
-
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
   );
 }
-
-export default Header;
-
