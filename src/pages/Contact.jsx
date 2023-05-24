bedcbdb52cdbe63b7f0a037d0e464413f6b1de2a
Ana Luisa <ana.luisa.isa@hotmail.com>
2023-05-23 21:26:37 -0300

    Feat[Contact]: Implementação da pagina de contatos, implementando o envio de emails

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,5 @@
-import analuisa from '../images/eu.jpeg';
-import imgFundo from '../images/img_fundo.png';
-
+import { useState, useRef } from 'react';
+import emailjs from '@emailjs/browser';
 import Header from '../components/Header';
 
 const navigation = [
@@ -11,56 +10,129 @@ const navigation = [
   { name: 'Contato', href: '/contato', current: true },
 ];
 
-function Contact() {
+export default function Contact() {
+  const formRef = useRef();
+
+  const [forms, setForms] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  function handleInputChange(event) {
+    const { name, value } = event.target;
+    setForms({ ...forms, [name]: value });
+  }
+
+  function handleFormSubmit(event) {
+    event.preventDefault();
+
+    if (forms.email === '') {
+      alert('Por favor, preencha o campo de e-mail.');
+    } else if (forms.name === '') {
+      alert('Por favor, preencha o campo de nome.');
+    } else {
+      emailjs.sendForm("service_dwer2ib", "template_5tmz16u", formRef.current, "Vo9scylhDvY5Ghu_P")
+        .then((result) => {
+          alert('SUCCESS!', result.status, result.text);
+        }, (error) => {
+          alert('FAILED...', error);
+        });
+
+      event.target.reset();
+    }
+  }
+
   return (
     <div>
       <Header navigation={navigation} />
       <div>
-        <img
-          src={imgFundo}
-          alt=""
-          className="absolute inset-0 -z-10 h-screen w-screen object-cover object-right md:object-center"
-        />
-      </div>
-      <div className="flex mt-5">
-        <img
-          className="m-auto h-40 w-40 rounded-full ring-1 ring-gray-300 shadow-lg shadow-gray-400"
-          src={analuisa}
-          alt="Ana Luisa"
-        />
-      </div>
-
-      <div className="relative px-6 pt-8 lg:px-8 bg-black bg-opacity-50 lg:mx-40 mt-6 rounded-3xl">
-
-        <div className="m-auto max-w-2xl ">
-
-          <div className="text-center">
-            <h1 className="text-4xl font-bold tracking-tight text-indigo-400 sm:text-5xl bg-clip-text text-transparent bg-gradient-to-r from-pink-400 via-purple-300 to-blue-500">
-              Prazer, sou Ana Luisa!
-            </h1>
-            <h4 className=" text-indigo-400 sm:text-3xl bg-clip-text text-transparent bg-gradient-to-r from-pink-400 via-purple-300 to-blue-500">
-              Desenvolvedora Web FullStack.
-            </h4>
-            <p className="mt-6 text-lg leading-8 text-white">
-              Sou uma pessoa colaborativa e comprometida em prestar um bom serviço. Gosto de trabalhar em equipe e ajudar no que for necessário. Ansiosa para aplicar minhas habilidades em projetos desafiadores.
-            </p>
-            <div className="mt-6 mb-6 flex items-center justify-center gap-x-6">
-              <a
-                href="https://github.com/analuisams99"
-                className="rounded-md bg-gray-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                GitHub
-              </a>
-              <a href="https://www.linkedin.com/in/analuisams99/" className="text-sm font-semibold leading-6 text-blue-400">
-                Linkedin <span aria-hidden="true">→</span>
-              </a>
-            </div>
+        <div className="px-6 py-8 sm:py-8 lg:px-8">
+          <div
+            className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
+            aria-hidden="true"
+          >
+            <div
+              className="relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]"
+              style={{
+                clipPath:
+                  'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+              }}
+            />
           </div>
+          <div className="mx-auto max-w-2xl text-center">
+            <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">Entre em contato</h2>
+            <p className="mt-2 text-lg leading-8 text-white">
+              Deixe seu recado e confira minhas redes. Vou adorar entrar em contato com você!
+            </p>
+          </div>
+
+          <form ref={formRef} onSubmit={handleFormSubmit} className="mx-auto max-w-xl sm:mt-8">
+            <div className="gap-x-8 gap-y-6 sm:grid-cols-2">
+              <div>
+                <label htmlFor="name" className="block text-sm font-semibold leading-6 text-gray-200">
+                  Nome/Empresa
+                </label>
+                <div className="mt-2.5">
+                  <input
+                    required
+                    onChange={handleInputChange}
+                    type="text"
+                    name="name"
+                    id="name"
+                    autoComplete="given-name"
+                    className="block w-full rounded-md border-0 px-3.5 py-2 
+                    bg-purple-300 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+              <div className="sm:col-span-2 mt-4">
+                <label htmlFor="email" className="block text-sm font-semibold leading-6 text-gray-200">
+                  Email
+                </label>
+                <div className="mt-2.5">
+                  <input
+                    required
+                    type="email"
+                    name="email"
+                    id="email"
+                    autoComplete="email"
+                    onChange={handleInputChange}
+                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 bg-purple-300"
+                  />
+                </div>
+              </div>
+
+              <div className="sm:col-span-2 mt-4">
+                <label htmlFor="message" className="block text-sm font-semibold leading-6 text-gray-200">
+                  Mensagem
+                </label>
+                <div className="mt-2.5">
+                  <textarea
+                    name="message"
+                    id="message"
+                    rows={4}
+                    className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset bg-purple-300 ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    defaultValue={''}
+                  />
+                </div>
+              </div>
+
+            </div>
+            <div className="mt-10">
+              <button
+                type="submit"
+
+                className="block w-full rounded-md bg-pink-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-600"
+              >
+                Enviar
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
 
-  );
+  )
 }
-
-export default Contact;
